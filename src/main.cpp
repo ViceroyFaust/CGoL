6ca89f93a6819a78921a93a8c782c28bf3cca4f9
Danylo Owl <d.ribchinski@gmail.com>
2021-09-01 23:46:22 -0400

    created set and gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,15 @@
 #include <stdio.h>
+#include <utility>
+#include <unordered_set>
+
+int xMax = 100;
+int yMax = 100;
+
+std::unordered_set<std::pair<int, int>> cells;
+
+void nextGen() {
+    
+}
 
 int main (/*int argc, char* argv[]*/) {
     printf("Hello World!");
