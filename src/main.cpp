4fcb118591fc0bf975292a64140bd0f3347a88b2
Danylo Owl <d.ribchinski@gmail.com>
2021-09-03 23:50:25 -0400

    changed system to use row-major array

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,14 @@
-#include <stdio.h>
-#include <algorithm>
-#include <utility>
-#include <vector>
+bool* grid{};
 
-int xMax = 100;
-int yMax = 100;
-
-int neighbours(const std::vector<std::pair<int, int>>& curGen, std::pair<int, int> cell) {
-    std::vector<std::pair<int, int>>::const_iterator directions[8] {
-    // (0,0) starts in the upper left corner
-    std::find(begin(curGen), end(curGen), std::pair(cell.first - 1, cell.second - 1)),
-    std::find(begin(curGen), end(curGen), std::pair(cell.first, cell.second - 1)),
-    std::find(begin(curGen), end(curGen), std::pair(cell.first + 1, cell.second - 1)),
-    std::find(begin(curGen), end(curGen), std::pair(cell.first + 1, cell.second)),
-    std::find(begin(curGen), end(curGen), std::pair(cell.first + 1, cell.second + 1)),
-    std::find(begin(curGen), end(curGen), std::pair(cell.first, cell.second + 1)),
-    std::find(begin(curGen), end(curGen), std::pair(cell.first - 1, cell.second + 1)),
-    std::find(begin(curGen), end(curGen), std::pair(cell.first - 1, cell.second))
-    };
-    int counter{0};
-    for (int i{0}; i < sizeof(directions)/sizeof(directions[0]); ++i) {
-        if (directions[i] != end(curGen))
-            counter++;
-    }
-    return counter;
+void init(int x, int y) {
+grid = new bool[x * y]{};
 }
 
-std::vector<std::pair<int, int>> nextGen(const std::vector<std::pair<int, int>>& curGen) {
-
+void clean() {
+    delete[] grid;
 }
 
 int main (/*int argc, char* argv[]*/) {
-    printf("Hello World!");
+    init(256, 256);
+    clean();
 }
