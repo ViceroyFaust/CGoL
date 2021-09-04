2e96094eeb047876fb343df22ce9f10d88417125
Danylo Owl <d.ribchinski@gmail.com>
2021-09-04 17:50:33 -0400

    first implementation of game of life

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,34 @@
-bool* grid{};
+#include <algorithm>
 
-void init(int x, int y) {
-grid = new bool[x * y]{};
-}
-
-void clean() {
-    delete[] grid;
-}
-
-int main (/*int argc, char* argv[]*/) {
-    init(256, 256);
-    clean();
+int main(/*int argc, char* argv[]*/) {
+    // Row-Major order. Formula is [col + num_columns * row]
+    int y = 256;
+    int x = 256;
+    int generations = 8;
+    bool* curGrid = new bool[y * x]();
+    bool* newGrid = new bool[y * x]();
+    for (int i = 0; i < generations; ++i) {
+        int neighbours = 0;
+        // Generation loop
+        for (int row = 1; row < y - 1; ++row) {
+            for (int col = 1; col < x - 1; ++col) {
+                for (int nRow = row - 1; nRow < row + 1; ++nRow) {
+                    for (int nCol = col - 1; nCol < col + 1; ++nCol) {
+                        if (nRow == row && nCol == col)
+                            continue;
+                        if (curGrid[nCol + x * nRow])
+                            ++neighbours;
+                    }
+                }
+                if (curGrid[col + x * row])
+                    if (neighbours == 2 || neighbours == 3)
+                        newGrid[col + x * row] = true;
+                    else if (neighbours == 3)
+                        newGrid[col + x * row] = true;
+                neighbours = 0;
+            }
+        }
+        std::swap(curGrid, newGrid);
+        std::fill(newGrid, newGrid + y * x, false);
+    }
 }
