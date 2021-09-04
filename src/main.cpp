6121f35f8da5230a2f1c3c97dd722857f38bfd84
Danylo Owl <d.ribchinski@gmail.com>
2021-09-04 18:42:14 -0400

    added pattern, condensed into main, and fixes

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,32 @@
 #include <algorithm>
+#include <stdio.h>
 
 int main(/*int argc, char* argv[]*/) {
     // Row-Major order. Formula is [col + num_columns * row]
     int y = 256;
     int x = 256;
-    int generations = 8;
+    int generations = 4;
     bool* curGrid = new bool[y * x]();
     bool* newGrid = new bool[y * x]();
+    // Constructing a 5x5 square with no corners
+    curGrid[5 + x * 3];
+    curGrid[6 + x * 3];
+    curGrid[7 + x * 3];
+    curGrid[4 + x * 4];
+    curGrid[8 + x * 4];
+    curGrid[4 + x * 5];
+    curGrid[8 + x * 5];
+    curGrid[4 + x * 6];
+    curGrid[8 + x * 6];
+    curGrid[5 + x * 7];
+    curGrid[6 + x * 7];
+    curGrid[7 + x * 7];
+    // Num of gen repetitions
     for (int i = 0; i < generations; ++i) {
-        int neighbours = 0;
         // Generation loop
         for (int row = 1; row < y - 1; ++row) {
             for (int col = 1; col < x - 1; ++col) {
+                int neighbours = 0;
                 for (int nRow = row - 1; nRow < row + 1; ++nRow) {
                     for (int nCol = col - 1; nCol < col + 1; ++nCol) {
                         if (nRow == row && nCol == col)
@@ -20,15 +35,17 @@ int main(/*int argc, char* argv[]*/) {
                             ++neighbours;
                     }
                 }
-                if (curGrid[col + x * row])
-                    if (neighbours == 2 || neighbours == 3)
-                        newGrid[col + x * row] = true;
-                    else if (neighbours == 3)
-                        newGrid[col + x * row] = true;
-                neighbours = 0;
+                if ((neighbours == 3) || (neighbours == 2 && curGrid[col + x * row]))
+                    newGrid[col + x * row] = true;
+                else
+                    newGrid[col + x * row] = false;
             }
         }
         std::swap(curGrid, newGrid);
-        std::fill(newGrid, newGrid + y * x, false);
     }
+    // print loop for all of the final live cell points
+    for (int row = 1; row < y - 1; ++row)
+        for (int col = 1; col < x - 1; ++col)
+            if (curGrid[col + x * row])
+                std::printf("(row: %d, col: %d\n", row, col);
 }
